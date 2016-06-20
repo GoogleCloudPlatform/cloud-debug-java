a37fa6fbbe9f151ef701a5fc601b919820b05c0e
gigid <gigid@google.com>
2016-06-20 15:17:50 -0700

    Preparing open source build to use mako to generate on the fly the labels.java source file and compile it. ------------- Created by MOE: https://github.com/google/moe MOE_MIGRATED_REVID=125385592

---
+++ src/agent/internals/src/main/java/com/google/devtools/clouddebugger/v2/Labels.java
@@ -0,0 +1,29 @@
+/**
+ * Defines the keys for the well known labels used by the cloud debugger.
+ *
+ * DO NOT EDIT - This file is auto-generated
+ */
+package com.google.devtools.clouddebugger.v2;
+
+public final class Labels {
+  public static final class Debuggee {
+
+    public static final String DOMAIN = "domain";
+    public static final String PROJECT_ID = "projectid";
+    public static final String VERSION = "version";
+    public static final String MODULE = "module";
+    public static final String MINOR_VERSION = "minorversion";
+
+    private Debuggee() {}
+  }
+
+  public static final class Breakpoint {
+
+    public static final String REQUEST_LOG_ID = "requestlogid";
+
+    private Breakpoint() {}
+  }
+
+
+  private Labels() {}
+}

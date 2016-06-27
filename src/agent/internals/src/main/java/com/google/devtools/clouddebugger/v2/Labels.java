b8e77fe0b93eaaf0523465c69ae495ee6ef8c7a1
emrekultursay <emrekultursay@google.com>
2016-06-27 16:12:41 -0700

    Clean moe_db.txt with direct equivalence mapping for the version 2.6 of Java Agent.
    Obtained via note_equivalence.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=126011210

--- src/agent/internals/src/main/java/com/google/devtools/clouddebugger/v2/Labels.java
+++ src/agent/internals/src/main/java/com/google/devtools/clouddebugger/v2/Labels.java
@@ -1,4 +1,20 @@
 /**
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
  * Defines the keys for the well known labels used by the cloud debugger.
  *
  * DO NOT EDIT - This file is auto-generated
@@ -8,11 +24,11 @@ package com.google.devtools.clouddebugger.v2;
 public final class Labels {
   public static final class Debuggee {
 
-    public static final String MINOR_VERSION = "minorversion";
     public static final String DOMAIN = "domain";
-    public static final String VERSION = "version";
-    public static final String MODULE = "module";
     public static final String PROJECT_ID = "projectid";
+    public static final String MODULE = "module";
+    public static final String MINOR_VERSION = "minorversion";
+    public static final String VERSION = "version";
 
     private Debuggee() {}
   }

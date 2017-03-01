5c3cb42b827fdf16de68b3b086b3ad89f9bea314
emrekultursay <emrekultursay@google.com>
2017-02-28 16:23:34 -0800

    Increment minor version to 2.9 to prepare for a new release.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=148830554

--- src/agent/internals/src/main/java/com/google/devtools/cdbg/debuglets/java/GcpDebugletVersion.java
+++ src/agent/internals/src/main/java/com/google/devtools/cdbg/debuglets/java/GcpDebugletVersion.java
@@ -31,7 +31,7 @@ public final class GcpDebugletVersion {
   /**
    * Minor version of the agent.
    */
-  public static final int MINOR_VERSION = 8;
+  public static final int MINOR_VERSION = 9;
 
   /**
    * Debugger agent version string in the format of MAJOR.MINOR.

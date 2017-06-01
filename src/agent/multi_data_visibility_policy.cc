e7befac4050cef91c100d637940bbb675d065879
mattwach <mattwach@google.com>
2017-06-01 11:09:26 -0700

    Implement MultiDataVisibilityPolicy.
      This visibility policy allows multiple policies to exist behind a single
      interface.  It enables us to support multiple policies in the code without
      adding knowledge/complication to the clients perspective.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=157735992

---
+++ src/agent/multi_data_visibility_policy.cc
@@ -0,0 +1,121 @@
+#include "multi_data_visibility_policy.h"
+
+namespace devtools {
+namespace cdbg {
+
+namespace {
+
+// Implementation of DataVisibilityPolicy::Class that is intended to hold
+// two or more DataVisibilityPolicy::Class child objects and return a
+// combination of thier results.
+//
+// The logic also supports the zero and one child case logically, but adds
+// some ineffieincies in regards to memory use and (slightly) cpu time.
+class ClassImpl : public DataVisibilityPolicy::Class {
+ public:
+  bool IsFieldVisible(const string& name, int32 field_modifiers) override {
+    for (const auto& policy : class_list_) {
+      if (!policy->IsFieldVisible(name, field_modifiers)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  bool IsMethodVisible(
+      const string& method_name,
+      const string& method_signature,
+      int32 method_modifiers) override {
+    for (const auto& policy : class_list_) {
+      if (!policy->IsMethodVisible(
+          method_name,
+          method_signature,
+          method_modifiers)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  bool IsVariableVisible(
+      const string& method_name,
+      const string& method_signature,
+      const string& variable_name) override {
+    for (const auto& policy : class_list_) {
+      if (!policy->IsVariableVisible(
+          method_name,
+          method_signature,
+          variable_name)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  // A list of class policies to consider.  Made public to simplify the
+  // GetClassVisibility() logic below (e.g. avoids additional logic that
+  // creates a separate vector and moves it's data into this class).
+  std::vector<std::unique_ptr<DataVisibilityPolicy::Class>> class_list_;
+};
+
+}  // namespace
+
+
+MultiDataVisibilityPolicy::MultiDataVisibilityPolicy(
+    std::unique_ptr<DataVisibilityPolicy> policy1,
+    std::unique_ptr<DataVisibilityPolicy> policy2) {
+  policy_list_.push_back(std::move(policy1));
+  policy_list_.push_back(std::move(policy2));
+}
+
+
+MultiDataVisibilityPolicy::MultiDataVisibilityPolicy(
+    std::unique_ptr<DataVisibilityPolicy> policy1,
+    std::unique_ptr<DataVisibilityPolicy> policy2,
+    std::unique_ptr<DataVisibilityPolicy> policy3) {
+  policy_list_.push_back(std::move(policy1));
+  policy_list_.push_back(std::move(policy2));
+  policy_list_.push_back(std::move(policy3));
+}
+
+
+std::unique_ptr<DataVisibilityPolicy::Class>
+MultiDataVisibilityPolicy::GetClassVisibility(jclass cls) {
+  // A few common cases avoid the need to store the ClassImpl wrapper, saving
+  // time and memory.
+  //
+  // 1) If every policy returns nullptr, this can return nullptr too - no need
+  //    to create a ClassImpl wrapper.  This is a *very common case*.
+  // 2) If everyone but one policy returns nullptr, the non-null
+  //    unique_ptr can be returned directly without the need for a ClassImpl
+  //    wrapper
+  std::unique_ptr<ClassImpl> class_impl(new ClassImpl());
+
+  for (const auto& policy : policy_list_) {
+    std::unique_ptr<DataVisibilityPolicy::Class> policy_class =
+        policy->GetClassVisibility(cls);
+
+    if (policy_class != nullptr) {
+      class_impl->class_list_.push_back(std::move(policy_class));
+    }
+  }
+
+  if (class_impl->class_list_.empty()) {
+    // No active policies, this class is always whitelisted.
+    return nullptr;
+  }
+
+  if (class_impl->class_list_.size() == 1) {
+    // Only one policy, no need to wrap it.
+    return std::move(class_impl->class_list_[0]);
+  }
+
+  // Multiple policies need to be considered
+  return std::move(class_impl);
+}
+
+}  // namespace cdbg
+}  // namespace devtools
---
+++ src/agent/multi_data_visibility_policy.h
@@ -0,0 +1,55 @@
+/**
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef DEVTOOLS_CDBG_DEBUGLETS_JAVA_MULTI_DATA_VISIBILITY_POLICY_H_
+#define DEVTOOLS_CDBG_DEBUGLETS_JAVA_MULTI_DATA_VISIBILITY_POLICY_H_
+
+#include <vector>
+#include "data_visibility_policy.h"
+
+namespace devtools {
+namespace cdbg {
+
+// Data visibility policy that can act as a frontend for two or three
+// backend policies.
+//
+// This simplifies the logic of client code, which can act as if there
+// is only a single policy.
+class MultiDataVisibilityPolicy : public DataVisibilityPolicy {
+ public:
+  // Build a policy with two child policies.  Takes ownership of arguments.
+  MultiDataVisibilityPolicy(
+      std::unique_ptr<DataVisibilityPolicy> policy1,
+      std::unique_ptr<DataVisibilityPolicy> policy2);
+
+  // Build a policy with three child policies.  Takes ownership of arguments.
+  MultiDataVisibilityPolicy(
+      std::unique_ptr<DataVisibilityPolicy> policy1,
+      std::unique_ptr<DataVisibilityPolicy> policy2,
+      std::unique_ptr<DataVisibilityPolicy> policy3);
+
+  std::unique_ptr<Class> GetClassVisibility(jclass cls) override;
+
+ private:
+  std::vector<std::unique_ptr<DataVisibilityPolicy>> policy_list_;
+
+  DISALLOW_COPY_AND_ASSIGN(MultiDataVisibilityPolicy);
+};
+
+}  // namespace cdbg
+}  // namespace devtools
+
+#endif  // DEVTOOLS_CDBG_DEBUGLETS_JAVA_MULTI_DATA_VISIBILITY_POLICY_H_

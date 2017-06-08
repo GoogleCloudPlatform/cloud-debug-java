c9517ad2fa2b7e5e428527ab19ad8ace1027bbc7
mattwach <mattwach@google.com>
2017-06-08 10:00:30 -0700

    Remove glob_data_visibility_policy dependency on internal RE2 library,   allowing it to be included in the open source file set.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=158407311

---
+++ src/agent/glob_data_visibility_policy.cc
@@ -0,0 +1,327 @@
+#include "glob_data_visibility_policy.h"
+
+#include <fnmatch.h>
+#include <algorithm>
+#include "type_util.h"
+
+namespace devtools {
+namespace cdbg {
+
+namespace {
+
+class ClassImpl : public DataVisibilityPolicy::Class {
+ public:
+  // Does not take ownership over "class_config", which must outlive this
+  // instance.
+  ClassImpl(
+      const string& class_name,
+      const GlobDataVisibilityPolicy::Config* config)
+      : class_name_(class_name),
+        config_(config) {
+  }
+
+  bool IsFieldVisible(
+      const string& field_name,
+      int32 field_modifiers) override {
+    return IsVisible(field_name);
+  }
+
+  bool IsMethodVisible(
+      const string& method_name,
+      const string& method_signature,
+      int32 method_modifiers) override {
+    return IsVisible(method_name);
+  }
+
+  bool IsVariableVisible(
+      const string& method_name,
+      const string& method_signature,
+      const string& variable_name) override {
+    return IsVisible(method_name + "." + variable_name);
+  }
+
+ private:
+  // Returns true if this class with the given suffix is visible.
+  // Example:
+  //   class_name_ = com.foo.MyClass
+  //   suffix = myMethod
+  //   checks: com.foo.MyClass.myMethod
+  bool IsVisible(const string& suffix) {
+    string path = class_name_ + "." + suffix;
+    return config_->whitelists.Matches(path) &&
+        !config_->blacklists.Matches(path);
+  }
+
+ private:
+  // Name of this class
+  const string class_name_;
+
+  // read-only pointer back to the global glob config
+  const GlobDataVisibilityPolicy::Config* config_;
+};
+
+
+// A simple implementation of DataVisibilityPolicy::Class which always
+// reports that methods and fields are invisible.
+class BlacklistedClassImpl : public DataVisibilityPolicy::Class {
+ public:
+  bool IsFieldVisible(const string& name, int32 field_modifiers) override {
+    return false;
+  }
+
+  bool IsMethodVisible(
+      const string& method_name,
+      const string& method_signature,
+      int32 method_modifiers) override {
+    return false;
+  }
+
+  bool IsVariableVisible(
+      const string& method_name,
+      const string& method_signature,
+      const string& variable_name) override {
+    return false;
+  }
+};
+
+}  // namespace
+
+std::unique_ptr<DataVisibilityPolicy::Class>
+GlobDataVisibilityPolicy::GetClassVisibility(jclass cls) {
+  string signature = GetClassSignature(cls);
+  if ((signature.size() < 3) ||
+      (signature.front() != 'L') || (signature.back() != ';')) {
+    return nullptr;  // Invalid class signature.
+  }
+
+  string path = TypeNameFromJObjectSignature(signature);
+  // replace $ with . in paths.  Without this replacement, someone
+  // could try and blacklist all class members with a rule like
+  //
+  // com.foo.MyClass.*
+  //
+  // but miss inner classes
+  //
+  // com.foo.MyClass$Inner (visible)
+  std::replace(path.begin(), path.end(), '$', '.');
+
+  // If neither this class nor it's members can ever be blacklisted,
+  // return nullptr as an optimization.
+  //
+  // Note that checking via whitelists.Matches here is only valid because
+  // we presume downward heirarchy inheritance (i.e. whitelisting a class
+  // also whitelists it's fields and members).  See GlobSet::Add
+  // for further details.
+  if (config_.whitelists.Matches(path) &&
+      !config_.blacklists.PrefixCanMatch(path)) {
+    return nullptr;
+  }
+
+  // If the class will always be blacklisted, return a BlacklistedClassImpl,
+  // which skips checks, saving time and memory for visibility checks on this
+  // class.
+  if (config_.blacklists.Matches(path) ||
+      !config_.whitelists.PrefixCanMatch(path)) {
+    return std::unique_ptr<Class>(new BlacklistedClassImpl());
+  }
+
+  return std::unique_ptr<Class>(new ClassImpl(path, &config_));
+}
+
+
+// Returns true if path can be matched by the wildcard pattern.
+static bool WildcardMatches(const string& path, const string& pattern) {
+  constexpr int kFlags = 0;
+  return fnmatch(pattern.c_str(), path.c_str(), kFlags) == 0;
+}
+
+
+// GenericMatches works in all cases but uses a slow O(n) algorithm.  Due to
+// it's performance, it's intended to process the generic_patterns_ set.
+static bool GenericMatches(
+    const string& path,
+    const std::set<string>& generic_match) {
+
+  for (const string& pattern : generic_match) {
+    if (WildcardMatches(path, pattern)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+
+// PrefixMatches uses a fast O(log n) algorithm to match globs that
+// end with *.
+//
+// path is the string to match
+//
+// prefix_match is a sorted set of prefixes with all redundant aliases
+// removed (i.e. with RemoveRedundantPrefixes).  Each pattern is implicitly
+// assumed to end with a *.  There should not be any '*' characters in the
+// actual data.
+static bool PrefixMatches(
+    const string& path,
+    const std::vector<string>& prefix_match) {
+  // Find the lower bound, but only consider the matching prefixes in
+  // each comparison
+  auto lower_bound = std::lower_bound(
+      prefix_match.begin(),
+      prefix_match.end(),
+      path,
+      [] (const string& prefix, const string& path) {
+        const size_t min_size = std::min(prefix.length(), path.length());
+        return path.compare(0, min_size, prefix) > 0;
+      });
+
+  // No lower bound was found, path must be lower than all existing prefixes
+  if (lower_bound == prefix_match.end()) {
+    return false;
+  }
+
+  // Return true only if the found lower bound actually is a prefix of path
+  return std::equal(lower_bound->begin(), lower_bound->end(), path.begin());
+}
+
+
+// Removes redundant prefixes in place.  Removing these is necessary for
+// PrefixMatches() to function correctly.
+//
+// The prefixes vector must be sorted before calling this function.
+//
+// For example, if searching for AB inside [A AA B], the AA redundant prefix
+// would incorrectly pivot the search toward B.  Removing the redundant
+// prefix resolves the issue.
+static void RemoveRedundantPrefixes(std::vector<string>* prefixes) {
+  if (prefixes->size() <= 1) {
+    // Nothing to do
+    return;
+  }
+  int base = 0;
+  for (int compare = 1; compare < prefixes->size(); ++compare) {
+    const bool keep_prefix = !std::equal(
+        prefixes->at(base).begin(),
+        prefixes->at(base).end(),
+        prefixes->at(compare).begin());
+
+    if (keep_prefix) {
+      // prefix becomes the new base
+      ++base;
+      // if compare and base are not together, the string
+      // needs to be moved back
+      if (compare > base) {
+        prefixes->at(base) = std::move(prefixes->at(compare));
+      }
+    }
+    // else keep base where it is and skip this prefix.
+    // The next keep_prefix will overwrite it
+  }
+  prefixes->resize(base + 1);
+}
+
+
+void GlobDataVisibilityPolicy::GlobSet::Prepare() {
+  std::sort(prefix_patterns_.begin(), prefix_patterns_.end());
+  RemoveRedundantPrefixes(&prefix_patterns_);
+  prepared_ = true;
+}
+
+
+bool GlobDataVisibilityPolicy::GlobSet::Matches(const string& path) const {
+  DCHECK(prepared_) << "Match() called before Prepare()";
+  if (exact_patterns_.find(path) != exact_patterns_.end()) {
+    return true;
+  }
+
+  if (PrefixMatches(path, prefix_patterns_)) {
+    return true;
+  }
+
+  return GenericMatches(path, generic_patterns_);
+}
+
+
+void GlobDataVisibilityPolicy::GlobSet::Add(const string& glob_pattern) {
+  prepared_ = false;
+
+  if (glob_pattern.empty()) {
+    return;
+  }
+
+  const size_t index = glob_pattern.find('*');
+
+  if (index == string::npos) {
+    exact_patterns_.insert(glob_pattern);
+
+    // All patterns that don't end with * must have '.*'
+    // added for consistent heirarchy propagation
+    //
+    // Otherwise, someone could, for example, blacklist a class:
+    //
+    // foo.bar.MyClass
+    //
+    // but methods, and variables in that class would still be visible:
+    //
+    // foo.bar.MyClass.MyVariable (visible)
+    // foo.bar.MyClass.MyMethod (visible)
+    // foo.far.MyClass$InnerClass (visible)
+    //
+    // which is both surprising and not useful.
+    prefix_patterns_.push_back(glob_pattern + ".");
+  } else if (index == glob_pattern.length() - 1) {
+    prefix_patterns_.push_back(
+        glob_pattern.substr(0, glob_pattern.length() - 1));
+  } else {
+    generic_patterns_.insert(glob_pattern);
+  }
+}
+
+
+// Returns true if it's possible for prefix* to match pattern.
+static bool PrefixCanMatchPattern(
+    const string& prefix,
+    const string& pattern) {
+  for (int i = 0; i < pattern.length() && i < prefix.length(); ++i) {
+    // It's possible for prefix to match pattern if either of the following
+    // are true:
+    //
+    // 1) prefix is a prefix of pattern
+    // 2) prefix[:n] is a prefix of pattern where pattern[n] is a '*'
+    if (pattern[i] == '*') {
+      return true;
+    }
+
+    if (pattern[i] != prefix[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+
+bool GlobDataVisibilityPolicy::GlobSet::PrefixCanMatch(
+    const string& prefix) const {
+  if (exact_patterns_.find(prefix) != exact_patterns_.end()) {
+    return true;
+  }
+
+  // TODO(mattwach): This can probably be done with more efficiency,
+  // but delay it for it's own CL.
+  for (const string& pattern : prefix_patterns_) {
+    if (PrefixCanMatchPattern(prefix, pattern + '*')) {
+      return true;
+    }
+  }
+
+  for (const string& pattern : generic_patterns_) {
+    if (PrefixCanMatchPattern(prefix, pattern)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+
+}  // namespace cdbg
+}  // namespace devtools
---
+++ src/agent/glob_data_visibility_policy.h
@@ -0,0 +1,106 @@
+/**
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef DEVTOOLS_CDBG_DEBUGLETS_JAVA_GLOB_DATA_VISIBILITY_POLICY_H_
+#define DEVTOOLS_CDBG_DEBUGLETS_JAVA_GLOB_DATA_VISIBILITY_POLICY_H_
+
+#include <set>
+#include "data_visibility_policy.h"
+
+namespace devtools {
+namespace cdbg {
+
+// Specifies lists of glob patterns that can act as whitelists and blacklists.
+class GlobDataVisibilityPolicy : public DataVisibilityPolicy {
+ public:
+  class GlobSet {
+   public:
+    // Adds a new pattern
+    void Add(const string& glob_pattern);
+
+    // Prepares the globset for matching.  Must be called once between calling
+    // Add and calling Match().
+    void Prepare();
+
+    // Returns true if at least one pattern in this GlobSet matches the given
+    // path.
+    bool Matches(const string& path) const;
+
+    // Returns true if at least one pattern in this GlobSet matches the prefix*
+    // pattern.
+    //
+    // For example, say the prefix is foo.bar.MyClass
+    //
+    // Presence any of the following glob patterns would return a "true" result
+    //
+    // foo.bar.MyClass
+    // foo.bar.MyClass.xyz
+    // *
+    // *anything
+    // foo*anything
+    //
+    // whereas presence of the following patterns can never match any path
+    // prefixed with foo.bar.MyClass
+    //
+    // foo.bar.YourClass
+    // java.util.*
+    bool PrefixCanMatch(const string& prefix) const;
+
+   private:
+    // Patterns that do not contain a *.  These can be resolved with a direct
+    // lookup.
+    std::set<string> exact_patterns_;
+
+    // Patterns that start with a string and end with a single *.  These can
+    // be efficiently resolved in O(log n) time
+    //
+    // Note: the '*' is not present in the data below.
+    // Note: This is a vector because std::lower_bound, used in the prefix match
+    // algorithm, requires a random-access container for O(log n) efficiency.
+    std::vector<string> prefix_patterns_;
+
+    // Patterns that contain one or more * characters and do not qualify for
+    // membership in prefix_patterns_ above.  Optimizing these further is
+    // possible, but seeminly in trade for additional complexity.  These
+    // patterns are also expected to be more rarely used than the other cases.
+    std::set<string> generic_patterns_;
+
+    // Set to true if the GlobSet is ready for calls to Match().
+    bool prepared_ = true;
+  };
+
+  // Configuration
+  struct Config {
+    GlobSet blacklists;
+    GlobSet whitelists;
+  };
+
+  explicit GlobDataVisibilityPolicy(Config config)
+      : config_(std::move(config)) {
+  }
+
+  std::unique_ptr<Class> GetClassVisibility(jclass cls) override;
+
+ private:
+  const Config config_;
+
+  DISALLOW_COPY_AND_ASSIGN(GlobDataVisibilityPolicy);
+};
+
+}  // namespace cdbg
+}  // namespace devtools
+
+#endif  // DEVTOOLS_CDBG_DEBUGLETS_JAVA_GLOB_DATA_VISIBILITY_POLICY_H_

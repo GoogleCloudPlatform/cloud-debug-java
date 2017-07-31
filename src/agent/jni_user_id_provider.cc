69f043b39874026bc5b84f4274e249beaa256a4e
emrekultursay <emrekultursay@google.com>
2017-07-31 16:49:50 -0700

    Add C++ interface and JNI-specific implemetation for user id provider.
    These classes are not used by the agent yet.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=170418398

---
+++ src/agent/jni_user_id_provider.cc
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "jni_user_id_provider.h"
+
+#include "jni_proxy_useridprovider.h"
+
+namespace devtools {
+namespace cdbg {
+
+JniUserIdProvider::JniUserIdProvider(std::function<JniLocalRef()> factory)
+    : factory_(std::move(factory)) {
+}
+
+
+void JniUserIdProvider::Collect() {
+  JniLocalRef provider = factory_();
+  if (provider == nullptr) {
+    LOG(WARNING) << "End user identity provider not available";
+    return;
+  }
+
+  DCHECK(jni()->IsInstanceOf(
+      provider.get(),
+      jniproxy::UserIdProvider()->GetClass()));
+
+  provider_ = JniNewGlobalRef(provider.get());
+}
+
+
+bool JniUserIdProvider::Format(string* kind, string* id) {
+  if (provider_ == nullptr) {
+    return false;  // User id not available.
+  }
+
+  auto rc = jniproxy::UserIdProvider()->format(provider_.get());
+  if (rc.HasException()) {
+    rc.LogException();
+    return false;  // Failed to obtain user id.
+  }
+
+  std::vector<string> result = JniToNativeStringArray(rc.GetData().get());
+  if (result.size() != 2) {
+    return false;
+  }
+
+
+  *kind = result[0];
+  *id = result[1];
+  return true;
+}
+
+}  // namespace cdbg
+}  // namespace devtools
---
+++ src/agent/jni_user_id_provider.h
@@ -0,0 +1,55 @@
+/**
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef DEVTOOLS_CDBG_DEBUGLETS_JAVA_JNI_USER_ID_PROVIDER_H_
+#define DEVTOOLS_CDBG_DEBUGLETS_JAVA_JNI_USER_ID_PROVIDER_H_
+
+#include "user_id_provider.h"
+
+#include "common.h"
+#include "jni_utils.h"
+
+namespace devtools {
+namespace cdbg {
+
+// Invokes com.google.devtools.cdbg.UserIdProvider to expose end user identity
+// to the debugger agent.
+class JniUserIdProvider : public UserIdProvider {
+ public:
+  // The "factory" callback creates a Java object implementing the
+  // com.google.devtools.cdbg.UserIdProvider interface.
+  explicit JniUserIdProvider(std::function<JniLocalRef()> factory);
+
+  void Collect() override;
+
+  bool Format(string* kind, string* id) override;
+
+ private:
+  // Callback that creates a Java object implementing the
+  // com.google.devtools.cdbg.UserIdProvider interface.
+  std::function<JniLocalRef()> factory_;
+
+  // Global reference to Java class implementing the
+  // com.google.devtools.cdbg.UserIdProvider interface.
+  JniGlobalRef provider_;
+
+  DISALLOW_COPY_AND_ASSIGN(JniUserIdProvider);
+};
+
+}  // namespace cdbg
+}  // namespace devtools
+
+#endif  // DEVTOOLS_CDBG_DEBUGLETS_JAVA_JNI_USER_ID_PROVIDER_H_

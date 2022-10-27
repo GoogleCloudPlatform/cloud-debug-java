e06c9b0bdea5f80deeec7bb60daa9bfceeb4c24e
jasonborg <48138260+jasonborg@users.noreply.github.com>
2022-10-27 09:32:15 -0400

    chore: Add java AgentLoggerTest (#52)

---
+++ src/agent/internals/src/test/java/com/google/devtools/cdbg/debuglets/java/AgentLoggerTest.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.google.devtools.cdbg.debuglets.java;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.io.IOException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class AgentLoggerTest {
+  private static native String pull();
+
+  static {
+    // See tests/agent/test_logger.cc
+    System.loadLibrary("test_logger");
+  }
+
+  @Test
+  public void info() {
+    AgentLogger.info("Hello");
+    assertThat(pull()).isEqualTo("[INFO] Hello\n");
+  }
+
+  @Test
+  public void warn() {
+    AgentLogger.warn("Hello");
+    assertThat(pull()).isEqualTo("[WARNING] Hello\n");
+  }
+
+  @Test
+  public void severe() {
+    AgentLogger.severe("Hello");
+    assertThat(pull()).isEqualTo("[ERROR] Hello\n");
+  }
+
+  @Test
+  public void infofmt() {
+    AgentLogger.infofmt("%d + %s", 15, "orange");
+    assertThat(pull()).isEqualTo("[INFO] 15 + orange\n");
+  }
+
+  @Test
+  public void warnfmt() {
+    AgentLogger.warnfmt("%d + %s", 15, "orange");
+    assertThat(pull()).isEqualTo("[WARNING] 15 + orange\n");
+  }
+
+  @Test
+  public void severefmt() {
+    AgentLogger.severefmt("%d + %s", 15, "orange");
+    assertThat(pull()).isEqualTo("[ERROR] 15 + orange\n");
+  }
+
+  @Test
+  public void infofmtWithException() {
+    AgentLogger.infofmt(new IOException(), "three = %d", 3);
+    assertThat(pull())
+        .containsMatch("^\\[INFO\\] three = 3\njava.io.IOException\n.*infofmtWithException");
+  }
+
+  @Test
+  public void warnfmtWithException() {
+    AgentLogger.warnfmt(new IOException(), "four = %d", 4);
+    assertThat(pull())
+        .containsMatch("^\\[WARNING\\] four = 4\njava.io.IOException\n.*warnfmtWithException");
+  }
+
+  @Test
+  public void severefmtWithException() {
+    AgentLogger.severefmt(new IOException(), "five = %d", 5);
+    assertThat(pull())
+        .containsMatch("^\\[ERROR\\] five = 5\njava.io.IOException\n.*severefmtWithException");
+  }
+
+  @Test
+  public void nullFormatString() {
+    AgentLogger.warnfmt(null, 15, "apple");
+    assertThat(pull()).isEqualTo("[WARNING] Failed to format message: \"null\"\n");
+  }
+
+  @Test
+  public void badFormatString() {
+    AgentLogger.warnfmt("a = %d", "no");
+    assertThat(pull()).isEqualTo("[WARNING] Failed to format message: \"a = %d\", args: [no]\n");
+  }
+
+  @Test
+  public void nullException() {
+    Throwable t = null;
+    AgentLogger.severefmt(t, "a = %d", 4);
+    assertThat(pull()).isEqualTo("[ERROR] a = 4\nFailed to format thrown exception\n");
+  }
+}
---
+++ tests/agent/test_logger.cc
@@ -0,0 +1,115 @@
+/**
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * This test logger is used by some of the Java agent unit tests under
+ * src/agent/internals/src/test/java/com/google/devtools/cdbg/debuglets/java
+ */
+
+#include <memory>
+#include <vector>
+
+#include "src/agent/common.h"
+#include "src/agent/jni_utils.h"
+#include "src/agent/mutex.h"
+
+using devtools::cdbg::JniToNativeString;
+using devtools::cdbg::JniToJavaString;
+
+static struct LogState {
+  absl::Mutex mu;
+  std::string log_buffer;
+} *g_state;
+
+extern "C" JNIEXPORT jint JNICALL
+JNI_OnLoad(JavaVM* vm, void* reserved) {
+  std::vector<char*> argv_vector;
+  argv_vector.push_back(const_cast<char*>("test_logger"));
+
+  int argc = argv_vector.size();
+  char** argv = &argv_vector[0];
+
+  g_state = new LogState;
+  return JNI_VERSION_1_6;
+}
+
+
+extern "C" void JNIEXPORT JNICALL
+JNI_OnUnLoad(JavaVM* jvm, void* reserved) {
+  delete g_state;
+}
+
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_google_devtools_cdbg_debuglets_java_AgentLogger_info(
+    JNIEnv* jni,
+    jclass cls,
+    jstring message) {
+  devtools::cdbg::set_thread_jni(jni);
+
+  absl::MutexLock lock(&g_state->mu);
+
+  g_state->log_buffer += "[INFO] ";
+  g_state->log_buffer += JniToNativeString(message);
+  g_state->log_buffer += "\n";
+}
+
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_google_devtools_cdbg_debuglets_java_AgentLogger_warn(
+    JNIEnv* jni,
+    jclass cls,
+    jstring message) {
+  devtools::cdbg::set_thread_jni(jni);
+
+  absl::MutexLock lock(&g_state->mu);
+
+  g_state->log_buffer += "[WARNING] ";
+  g_state->log_buffer += JniToNativeString(message);
+  g_state->log_buffer += "\n";
+}
+
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_google_devtools_cdbg_debuglets_java_AgentLogger_severe(
+    JNIEnv* jni,
+    jclass cls,
+    jstring message) {
+  devtools::cdbg::set_thread_jni(jni);
+
+  absl::MutexLock lock(&g_state->mu);
+
+  g_state->log_buffer += "[ERROR] ";
+  g_state->log_buffer += JniToNativeString(message);
+  g_state->log_buffer += "\n";
+}
+
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_google_devtools_cdbg_debuglets_java_AgentLoggerTest_pull(
+    JNIEnv* jni,
+    jclass cls,
+    jstring message) {
+  devtools::cdbg::set_thread_jni(jni);
+
+  std::string prev;
+  {
+    absl::MutexLock lock(&g_state->mu);
+    g_state->log_buffer.swap(prev);
+  }
+
+  return static_cast<jstring>(JniToJavaString(prev).release());
+}
